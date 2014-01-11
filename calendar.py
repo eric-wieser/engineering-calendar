d17835e4afcf7321105d3f4e6cd4b05a200017a2
Eric Wieser <wieser.eric@gmail.com>
2014-01-11 14:58:33 +0000

    Adds server to serve corrected ical

--- calendar.py
+++ calendar.py
@@ -4,30 +4,19 @@ import re
 
 pattern = re.compile(r'(.*\])(.*)\((.*)\)')
 
+def fix(ical_string):
+	cal = icalendar.Calendar.from_ical(ical_string)
 
-cal_url = "http://td.eng.cam.ac.uk/tod/public/view_ical.php?yearval=2013_14&term=L&course=IA"
+	for event in cal.subcomponents:
+		if not isinstance(event, icalendar.Event):
+			print event
+			continue
 
-response = urllib2.urlopen(cal_url)
-cal = response.read()
-cal = icalendar.Calendar.from_ical(cal)
-with open('lecturesold.ics', 'wb') as f:
-	f.write(cal.to_ical())
+		m = pattern.match(event['summary'])
+		name, speaker, location = m.groups()
 
+		event['summary'] = icalendar.vText(name)
+		event['location'] = icalendar.vText(location)
+		event['description'] = icalendar.vText(speaker)
 
-for event in cal.subcomponents:
-	if not isinstance(event, icalendar.Event):
-		print event
-		continue
-
-	m = pattern.match(event['summary'])
-	name, speaker, location = m.groups()
-
-	if 'see rota' in name:
-		print name
-
-	event['summary'] = icalendar.vText(name)
-	event['location'] = icalendar.vText(location)
-	event['description'] = icalendar.vText(speaker)
-
-with open('lectures.ics', 'wb') as f:
-	f.write(cal.to_ical())
+	return cal.to_ical()
---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+import urllib2
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+import calendar
+
+cal_url = "http://td.eng.cam.ac.uk/tod/public/view_ical.php?yearval=2013_14&term=L&course=IA"
+
+class CalendarHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/calendar")
+		self.end_headers()
+
+		response = urllib2.urlopen(cal_url)
+		cal = response.read()
+
+		self.wfile.write(calendar.fix(cal))
+
+HOST_NAME = '127.0.0.1' #efw.srcf.net'
+PORT_NUMBER = 8080
+
+if __name__ == '__main__':
+	httpd = HTTPServer((HOST_NAME, PORT_NUMBER), CalendarHandler)
+	print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+	try:
+		httpd.serve_forever()
+	except KeyboardInterrupt:
+		pass
+	httpd.server_close()
+	print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
