7a7338ae5c389cfc9ac1f627b93bf91394deda83
Eric Wieser <wieser.eric@gmail.com>
2014-01-13 15:34:54 +0000

    Bug fix!

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import urllib2
 
-from bottle import route, run, template
+from bottle import route, run, template, response
 
 import calendar
 
 cal_url = "http://td.eng.cam.ac.uk/tod/public/view_ical.php?yearval=2013_14&term=L&course=IA"
 
 @route('/')
-def index(name):
+def index():
 	response.content_type = 'text/calendar'
-	response = urllib2.urlopen(cal_url)
-	cal = response.read()
+	cal_req = urllib2.urlopen(cal_url)
+	cal = cal_req.read()
 
 	return calendar.fix(cal)
 
-run(host='efw.user.srcf.net', port=8080)
+run(host='efw27.user.srcf.net', port=8080)
