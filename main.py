b12b438852f78aadd009eb6a2d5d08ab6d54cfcd
Eric Wieser <wieser.eric@gmail.com>
2014-01-13 15:30:57 +0000

    Switch to bottle

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
-import time
 import urllib2
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+from bottle import route, run, template
 
 import calendar
 
 cal_url = "http://td.eng.cam.ac.uk/tod/public/view_ical.php?yearval=2013_14&term=L&course=IA"
 
-class CalendarHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/calendar")
-		self.end_headers()
+@route('/')
+def index(name):
+	response.content_type = 'text/calendar'
+	response = urllib2.urlopen(cal_url)
+	cal = response.read()
 
-		response = urllib2.urlopen(cal_url)
-		cal = response.read()
+	return calendar.fix(cal)
 
-		self.wfile.write(calendar.fix(cal))
-
-HOST_NAME = 'efw.user.srcf.net'
-PORT_NUMBER = 8080
-
-if __name__ == '__main__':
-	httpd = HTTPServer((HOST_NAME, PORT_NUMBER), CalendarHandler)
-	print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
-	try:
-		httpd.serve_forever()
-	except KeyboardInterrupt:
-		pass
-	httpd.server_close()
-	print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+run(host='efw.user.srcf.net', port=8080)
