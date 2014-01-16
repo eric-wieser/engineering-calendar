853c94d3395194131dea51aa882e529f17e9e9b9
Eric Wieser <wieser.eric@gmail.com>
2014-01-16 12:24:10 +0000

    Fix module reference

--- calendar.py
+++ calendar.py
@@ -42,7 +42,7 @@ def fix(ical_string, lab_group=None):
 				del data[id(event)]
 
 		# add the labs
-		for l in labs.lab_events(terms.lent, lab_group):
+		for l in labs.lab_events(terms.IA.lent, lab_group):
 			event = icalendar.Event()
 			event['summary']  = icalendar.vText(l.info.name)
 			event['location'] = icalendar.vText(l.info.location)
