015655381353f7b2bb6168b895537a555f93772b
Eric Wieser <wieser.eric@gmail.com>
2014-03-30 20:14:51 +0100

    Move parse_row logic to associated user

--- timetable.py
+++ timetable.py
@@ -1,27 +1,53 @@
-from labs import parse_row
+import itertools
+import re
+
+parens = ('[]', '{}', '[]')
 
 class Timetable(object):
 	@staticmethod
 	def _str_to_key(s):
 		return tuple(map(int, s.split('-')))
 
+	@staticmethod
+	def _parse_row(row):
+		# deal with parens replacing week barriers
+		for start, end in parens:
+			row = row.replace(start, '|' + start)
+			row = row.replace(end, end + '|')
+
+		weeks = row.split('|')
+		weeks = weeks[1:-1]
+
+		data = []
+		sd_week = None
+
+		for i, week in enumerate(weeks):
+			if week.startswith('['):
+				assert week.endswith(']')
+				week = week[1:-1]
+				sd_week = i
+			labs = [l.strip() for l in week.split(',')]
+			data.append(labs)
+
+		return data, sd_week
+
 	def __init__(self, start_date, d):
 		self.start_date = start_date
 
-		l = None
+		self.num_days = None
 
 		self.d = {}
 		for k, v in d.iteritems():
 			k = self._str_to_key(k)
-			v = parse_row(v)
+			v = self._parse_row(v)
 
 			self.d[k] = v
 
 			# check rows are the same length
-			if l is not None and l != len(v):
+			if self.num_days is not None and self.num_days != len(v):
 				raise ValueError("Not all groups have the same length timetable!")
 
-			l = len(v)
+			self.num_days = len(v)
 
 	def __getitem__(self, key):
 		if isinstance(key, basestring):
@@ -37,4 +63,4 @@ class Timetable(object):
 
 	@property
 	def groups(self):
-	    return  sorted(self.d.keys())
+		return  sorted(self.d.keys())
